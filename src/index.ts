39f52ed87a3e056039962e771c3caddb57a86ea8
John Letey <johnletey@gmail.com>
2020-10-17 20:23:22 +0100

    feat: add the remaining `Storage` functions

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,78 @@
 import * as fs from "fs";
 
 export default class localPorridge {
-  public location!: string;
+  private location!: string;
+  public length: number;
 
   constructor(location: string) {
     this.location = location;
+
+    try {
+      const storage = this.getStorage();
+      this.length = Object.keys(storage).length;
+    } catch (err) {
+      this.length = 0;
+    }
+  }
+
+  key(index: number): string {
+    const storage = this.getStorage();
+    const keys = Object.keys(storage);
+    return storage[keys[index]];
   }
 
   getItem(name: string): string | undefined {
-    let content: string;
-
     try {
-      content = fs.readFileSync(this.location, {
-        encoding: "utf-8",
-      });
+      const storage = this.getStorage();
+      return storage[name];
     } catch (err) {
       return undefined;
     }
-
-    const storage: Record<string, string> = JSON.parse(content);
-
-    return storage[name];
   }
 
   setItem(name: string, value: string): void {
-    let content: string;
-
+    let storage: Record<string, string>;
     try {
-      content = fs.readFileSync(this.location, {
-        encoding: "utf-8",
-      });
+      storage = this.getStorage();
     } catch (err) {
-      content = "{}";
+      storage = {};
     }
 
-    const storage: Record<string, string> = JSON.parse(content);
-
+    if (!(name in storage)) {
+      this.length++;
+    }
     storage[name] = value;
 
     fs.writeFileSync(this.location, JSON.stringify(storage));
+  }
+
+  removeItem(name: string): void {
+    let storage: Record<string, string>;
+    try {
+      storage = this.getStorage();
+    } catch (err) {
+      storage = {};
+    }
+
+    if (name in storage) {
+      this.length--;
+    }
+    delete storage[name];
+
+    fs.writeFileSync(this.location, JSON.stringify(storage));
+  }
+
+  clear(): void {
+    fs.unlinkSync(this.location);
+  }
+
+  //
+
+  private getStorage(): Record<string, string> {
+    const content = fs.readFileSync(this.location, {
+      encoding: "utf-8",
+    });
+
+    return JSON.parse(content);
   }
 }
