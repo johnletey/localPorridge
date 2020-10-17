5ab7d84c2c6c4562bd22a3b3206d255a4def1beb
John Letey <johnletey@gmail.com>
2020-10-17 20:32:13 +0100

    chore: finish tests

--- src/tests.ts
+++ src/tests.ts
@@ -13,9 +13,26 @@ describe("E2E Tests", function () {
     localStorage.setItem("test", "1234");
     done();
   });
+  it("Test .length", (done) => {
+    const length = localStorage.length;
+    done();
+  });
   it("Test getItem(<NAME>)", (done) => {
     const value = localStorage.getItem("test");
     assert(value);
+    done();
+  });
+  it("Test key(<INDEX>)", (done) => {
+    const value = localStorage.key(0);
+    assert(value);
+    done();
+  });
+  it("Test removeItem(<NAME>)", (done) => {
+    localStorage.removeItem("test");
+    done();
+  });
+  it("Test clear()", (done) => {
+    localStorage.clear();
     done();
   });
 });
